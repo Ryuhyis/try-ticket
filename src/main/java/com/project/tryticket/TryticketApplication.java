fdcff1c64a107317ed920e1d4ad20cab6af02a76
Ryuhyis <skyblue22756@gmail.com>
2023-09-06 23:15:23 +0900

    Initial commit

---
+++ src/main/java/com/project/tryticket/TryticketApplication.java
@@ -0,0 +1,13 @@
+package com.project.tryticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/tryticket/TryticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tryticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
