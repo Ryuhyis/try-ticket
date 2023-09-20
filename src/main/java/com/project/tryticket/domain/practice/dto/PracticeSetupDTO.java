c263d6e14c110e47588c8ac99876d3474f84e23a
Ryuhyis <skyblue22756@gmail.com>
2023-09-20 16:41:01 +0900

    feat: Add Practice Domain Structure

---
+++ src/main/java/com/project/tryticket/domain/practice/dto/PracticeSetupDTO.java
@@ -0,0 +1,9 @@
+package com.project.tryticket.domain.practice.dto;
+
+import java.time.LocalDateTime;
+
+public class PracticeSetupDTO {
+   private String userID;
+   private String difficultyLevel;
+   private int startTime;
+}
